92e198751619a08da6e3c19960ce9115add10bbd
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-08-27 11:59:53 +0900

    feat: バリデーションのスキーマを作成

---
+++ api/schemas.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class RecipeBase(BaseModel):
+    temperature: int
+
+
+class RecipeCreate(RecipeBase):
+    pass
+
+
+class Recipe(RecipeBase):
+    id: int
+    creator_id: int
+    created: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    name: str
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    recipes: list[Recipe] = []
+    created: datetime
+    edited: datetime
+
+    class Config:
+        orm_mode = True
