b6f96255ff2cce5d9564124cbd388fcc3cacc8cb
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-08-29 18:35:30 +0900

    chore: web appを作成

---
+++ web/pages/index.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+
+import { 
+  Container,
+  CssBaseline,
+} from '@mui/material';
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>coffee</title>
+        {/* Responsive meta tag */}
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+        {/* Roboto font */}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        {/* Font icons */}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </Head>
+      <CssBaseline />
+      <Container>
+        hoge
+      </Container>
+    </>
+  )
+}
