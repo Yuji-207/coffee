cda6bccca29d7dfc2168d06f7d3b7d43300540d9
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-08-31 20:10:31 +0900

    feat: /homeを実装

--- web/pages/index.jsx
+++ web/pages/index.jsx
@@ -1,50 +1,125 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 
 import Head from 'next/head'
 
 import {
+  Button,
   Container,
   CssBaseline,
   Divider,
+  FormControl,
+  InputLabel,
   List,
   ListItem,
   ListItemButton,
   ListItemText,
+  MenuItem,
+  Select,
+  TextField,
   Typography,
 } from '@mui/material';
 
-import BottomBar from '../sections/BottomBar';
+import BottomBar from '../../sections/BottomBar';
 
-
-const items = [
-  {
-    beans: 'キリマンジャロ',
-    temperature: 92,
-    taste: 3,
-    strength: 3,
-  },
-  {
-    beans: 'マンデリン',
-    temperature: 88,
-    taste: 5,
-    strength: 5,
-  },
-  {
-    beans: 'モカ・シダモ',
-    temperature: 94,
-    taste: 2,
-    strength: 2,
-  },
-];
+const axios = require('axios');
+axios.defaults.baseURL = 'http://127.0.0.1:8000';
 
 
 export default function Home() {
 
+  const [distributions, setDistributions] = useState({});
+  const [newItem, setNewItem] = useState({user_id: 1});
+  const [recipes, setRecipes] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
 
+  const handleChange = event => {
+    const target = event.target;
+    const newValue = {...newItem};
+    newValue[target.name] = Number(target.value);
+    setNewItem(newValue);
+  };
+  
   const handleClick = () => {
     setModalOpen(!modalOpen);
   }
+
+  console.log({items})
+  useEffect(() => {
+    if (!modalOpen) {
+
+      axios.get('/users/distributions')
+        .then(res => {
+          setDistributions({
+            ...distributions,
+            users_taste: {
+              name: 'コーヒーの嗜好（酸味・苦味, すべてのユーザー）',
+              values: res.data.taste,
+            },
+            users_strength: {
+              name: 'コーヒーの嗜好（濃度, すべてのユーザー）',
+              values: res.data.strength,
+            },
+          });
+        })
+        .catch(error => {
+          console.log({error});
+        });
+
+      axios.get('/recipes/distributions')
+        .then(res => {
+          setDistributions({
+            ...distributions,
+            recipes_taste: {
+              name: 'レシピの傾向（酸味・苦味, すべてのユーザー）',
+              values: res.data.taste,
+            },
+            recipes_strength: {
+              name: 'レシピの傾向（濃度, すべてのユーザー）',
+              values: res.data.strength,
+            },
+          });
+        })
+        .catch(error => {
+          console.log({error});
+        });
+
+    } else {
+
+      axios.get('/recipes')
+        .then(recipesResponse => {
+          const tmp = recipesResponse.data;
+          for (let i = 0; i < tmp.length; i++) {
+
+            axios.get('/beans_list/' + tmp[i].beans_id)
+              .then(beansResponse => {
+                tmp[i]['beans'] = beansResponse.data;
+              })
+              .catch(error => {
+                console.log({error});
+              })
+              .finally(() => {
+                setRecipes(JSON.parse(JSON.stringify(tmp)));
+              });
+
+          }
+        })
+        .catch(error => {
+          console.log({error});
+        });
+
+    }
+  }, [modalOpen]);
+
+  const handleSave = () => {
+    axios.post(`/evaluations?user_id=${newItem.user_id}&recipe_id=${newItem.recipe_id}`, newItem)
+      .then(response => {
+        setModalOpen(false);
+      })
+      .catch(error => {
+        console.log({error});
+      });
+  };
+
   return (
     <>
       <Head>
@@ -66,31 +141,71 @@ export default function Home() {
       <CssBaseline />
       <Container sx={{py: 2}} fixed>
         {modalOpen ? (
-          <></>
+          <>
+            <Typography variant="subtitle1">レシピを評価してください</Typography>
+            <FormControl fullWidth>
+              <InputLabel id="select-label" sx={{my: 2}}>レシピ</InputLabel>
+              <Select
+                labelId="select-label"
+                label="レシピ"
+                name="recipe_id"
+                onChange={handleChange}
+                sx={{my: 2}}
+              >
+                {recipes.map(recipe => (
+                  <MenuItem key={recipe.id} value={recipe.id}>
+                    {recipe.beans !== undefined && (
+                      `${recipe.beans.name}：${recipe.temperature} ℃`
+                    )}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <TextField
+              name="taste"
+              label="酸味（1） - 苦味（5）"
+              type="number"
+              variant="outlined"
+              sx={{minWidth: 1, my: 2}}
+              onChange={handleChange}
+            />
+            <TextField
+              name="strength"
+              label="濃度（1 - 5）"
+              type="number"
+              variant="outlined"
+              sx={{minWidth: 1, my: 2}}
+              onChange={handleChange}
+            />
+            <Button
+              variant="contained"
+              onClick={handleSave}
+              sx={{my: 3}}
+              fullWidth
+            >
+              保存
+            </Button>
+          </>
         ) : (
           <List disablePadding>
-          {items.map((item, i) => (
-            <Fragment key={i} >
-              {i > 0 && (
-                <Divider component="li" />
-              )}
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="#simple-list">
-                  <ListItemText
-                    primary={item.beans}
-                    secondary={
-                      <>
-                        <Typography>温度：{item.temperature} ℃</Typography>
-                        <Typography>味わい：{item.taste}</Typography>
-                        <Typography>濃さ：{item.strength}</Typography>
-                      </>
-                    }
-                  />
-                </ListItemButton>
-              </ListItem>
-            </Fragment>
-          ))}
-        </List>
+            {distributions.map((distribution, i) => (
+              <Fragment key={distribution.id} >
+                {i > 0 && (
+                  <Divider component="li" />
+                )}
+                <ListItem key={i} disablePadding>
+                  <ListItemButton component="a" href="#simple-list">
+                    <ListItemText
+                      primary={distribution.name}
+                      secondary={
+                        <Typography>{distribution.values}</Typography>
+                      }
+                    />
+                  </ListItemButton>
+                </ListItem>
+              </Fragment>
+            ))}
+          </List>
         )}
       </Container>
       <BottomBar open={modalOpen} onClick={handleClick} />
