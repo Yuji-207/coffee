77cfcc06439b9c70d57504b7b2c315a09bb92b2f
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-08-31 20:13:15 +0900

    fix: 関数名を修正

--- api/main.py
+++ api/main.py
@@ -51,16 +51,8 @@ def read_users(skip: int=0, limit: int=100, db: Session=Depends(get_db)):
     return users
 
 
-@app.get('/users/{user_id}', response_model=schemas.User)
-def read_user(user_id: int, db: Session=Depends(get_db)):
-    db_user = crud.get_user(db, id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail='User not found')
-    return db_user
-
-
 @app.get('/users/distributions', response_model=schemas.Distribution)
-def read_user_distributions(db: Session=Depends(get_db)):
+def read_users_distributions(db: Session=Depends(get_db)):
 
     # user-recipes distributions and probabilities.
     taste = bayse.create_prior()
@@ -80,6 +72,14 @@ def read_user_distributions(db: Session=Depends(get_db)):
     }
 
     return distributions
+
+
+@app.get('/users/{user_id}', response_model=schemas.User)
+def read_user(user_id: int, db: Session=Depends(get_db)):
+    db_user = crud.get_user(db, id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail='User not found')
+    return db_user
 
 
 @app.post('/beans_list', response_model=schemas.Beans)
